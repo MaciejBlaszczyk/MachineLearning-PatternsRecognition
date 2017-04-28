9b25ecbab9b7364c4f188e636679e323008aa254
OptimusPrime <maciejblaszczyk1996@gmail.com>
2017-04-28 13:13:34 +0200

    comments added

--- Week1/KNN.py
+++ Week1/KNN.py
@@ -2,41 +2,33 @@ from sklearn import datasets
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import distance
-from matplotlib.colors import ListedColormap
 
 iris = datasets.load_iris()
-points = np.array(iris.data[ : , :2])
+points = np.array(iris.data[:, :2])
 types = iris.target
 
+# prepare arrays
+EucK1 = np.array(types)
+EucK5 = np.array(types)
+ManK1 = np.array(types)
+ManK5 = np.array(types)
 
-EucK1noCNN = np.array(types)
-EucK5noCNN = np.array(types)
-ManK1noCNN = np.array(types)
-ManK5noCNN = np.array(types)
-
-
-plt.figure(1)
-plt.scatter(points[:, 0], points[:, 1], c=types)
-plt.title('Data Set')
+# calculate distances between points (euclidean metric) and exchange zeros with nans
 distancesEuc = np.array(distance.cdist(points, points, 'euclidean'))
 distancesEuc[distancesEuc == 0] = np.nan
+
+# calculate distances between points (manhattan metric) and exchange zeros with nans
 distancesMan = np.array(distance.cdist(points, points, 'cityblock'))
 distancesMan[distancesMan == 0] = np.nan
 
-
+# perform classification (K = 1)
 for i in range(len(points)):
     minIndex = np.nanargmin(distancesEuc[i])
-    EucK1noCNN[i] = types[minIndex]
+    EucK1[i] = types[minIndex]
     minIndex = np.nanargmin(distancesMan[i])
-    ManK1noCNN[i] = types[minIndex]
-plt.figure(2)
-plt.scatter(points[:, 0], points[:, 1], c=EucK1noCNN)
-plt.title('Euclidean 1 NN')
-plt.figure(3)
-plt.scatter(points[:, 0], points[:, 1], c=ManK1noCNN)
-plt.title('Manhattan 1 NN')
+    ManK1[i] = types[minIndex]
 
-
+# perform classification (K = 5)
 for i in range(len(points)):
     tempResults = []
     for _ in range(5):
@@ -44,13 +36,13 @@ for i in range(len(points)):
         distancesEuc[i][minIndex] = np.nan
         tempResults.append(types[minIndex])
     if tempResults.count(0) > 2:
-        EucK5noCNN[i] = 0
+        EucK5[i] = 0
     elif tempResults.count(1) > 2:
-        EucK5noCNN[i] = 1
+        EucK5[i] = 1
     elif tempResults.count(2) > 2:
-        EucK5noCNN[i] = 2
+        EucK5[i] = 2
     else:
-        EucK5noCNN[i] = 3
+        EucK5[i] = 3
 
     tempResults = []
     for _ in range(5):
@@ -58,21 +50,28 @@ for i in range(len(points)):
         distancesMan[i][minIndex] = np.nan
         tempResults.append(types[minIndex])
     if tempResults.count(0) > 2:
-        ManK5noCNN[i] = 0
+        ManK5[i] = 0
     elif tempResults.count(1) > 2:
-        ManK5noCNN[i] = 1
+        ManK5[i] = 1
     elif tempResults.count(2) > 2:
-        ManK5noCNN[i] = 2
+        ManK5[i] = 2
     else:
-        ManK5noCNN[i] = 3
+        ManK5[i] = 3
 
+plt.figure(1)
+plt.scatter(points[:, 0], points[:, 1], c=types)
+plt.title('Data Set')
+plt.figure(2)
+plt.scatter(points[:, 0], points[:, 1], c=EucK1)
+plt.title('Euclidean 1 NN')
+plt.figure(3)
+plt.scatter(points[:, 0], points[:, 1], c=ManK1)
+plt.title('Manhattan 1 NN')
 plt.figure(4)
-plt.scatter(points[:, 0], points[:, 1], c=EucK5noCNN)
+plt.scatter(points[:, 0], points[:, 1], c=EucK5)
 plt.title("Euclidean 5 NN")
 plt.figure(5)
-plt.scatter(points[:, 0], points[:, 1], c=ManK5noCNN)
+plt.scatter(points[:, 0], points[:, 1], c=ManK5)
 plt.title('Manhattan 5 NN')
-print(ManK5noCNN)
+
 plt.show()
-
-
