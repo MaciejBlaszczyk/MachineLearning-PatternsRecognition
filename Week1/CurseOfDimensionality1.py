3690584f54e11131d39b05a8de7530f39575385b
OptimusPrime <maciejblaszczyk1996@gmail.com>
2017-04-28 12:51:55 +0200

    comments added

--- Week1/CurseOfDimensionality1.py
+++ Week1/CurseOfDimensionality1.py
@@ -1,25 +1,26 @@
-#The aim of the assignment is to investigate how the following quantities change depending on the number of dimensions. For the purposes of this task we use the Euclidean distance.
-
-# We have a hyperball with a radius equal to 1.0 inscribed inside a hypercube with edges length equal to 2.0.
-# Hyperball in multidimensional space is defined as a set of points whose distance from its centre is no greater than its radius.
-# We randomly fill the hypercube with evenly distributed points. What % of those points would be inside the hyperball, and what % outside – in the corners?
-
-
 import numpy as np
 import math
 import matplotlib.pyplot as plt
 
 numberOfPoints = 1000
 numberOfDimensions = 21
+hyperCubeEdge = 2
+hyperBallRadius = 1
+
 percentResults = []
 
+
 for dimensions in range(2, numberOfDimensions):
+    # generate random points in hypercube
     points = np.array([np.random.uniform(-1, 1, size = dimensions) for _ in range(numberOfPoints)])
 
+    # squared points to calculate distances
     squaredPoints = points ** 2
 
+    # calculate distances between points
     distances = np.array([math.sqrt(math.fsum(squaredPoints[i])) for i in range(numberOfPoints)])
 
+    # find points which are inside hyperball
     results = np.array([1 if distances[i] < 1 else 0 for i in range(numberOfPoints)])
 
     percent = (math.fsum(results)/numberOfPoints)*100
