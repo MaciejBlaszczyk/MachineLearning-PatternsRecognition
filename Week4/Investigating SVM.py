6ebbca89bbfeda46df70d9a30ac3d08b8a38ed7b
OptimusPrime <maciejblaszczyk1996@gmail.com>
2017-04-28 15:56:05 +0200

    comments added

--- Week4/Investigating SVM.py
+++ Week4/Investigating SVM.py
@@ -1,9 +1,3 @@
-# Generate a distribution of data points similar to the one on the picture (hint for lazy ones: the distribution can be painted in any graphical
-# editor using the paintbrush tool and then reconstructed by reading pixel colours).
-# For this distribution investigate how the decision boundary (the same one we observed in k-NN assignment)
-# will behave depending on the C coefficient and whether or not
-# the RBF kernel was employed (with different gamma coefficients). Check the margin width for each of those cases.
-
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.svm import SVC
@@ -16,9 +10,6 @@ data = np.concatenate([dataLeft, dataCircle, dataRight])
 
 # Array of labels
 target = [1 if i < 220 else 0 for i in range(420)]
-
-
-
 
 xMin, xMax = data[:, 0].min() - 1, data[:, 0].max() + 1
 yMin, yMax = data[:, 1].min() - 1, data[:, 1].max() + 1
@@ -39,7 +30,6 @@ for ker in kernel:
             plt.scatter(data[:, 0], data[:, 1], c=target)
             plt.title('Kernel: ' + ker + ' Gamma: ' + str(g) + ' C: ' + str(c))
 
-
 svc = SVC(kernel = 'linear')
 svc.fit(data, target)
 result = svc.predict(np.c_[xArray.ravel(), yArray.ravel()])
@@ -48,8 +38,5 @@ plt.figure()
 plt.contourf(xArray, yArray, result, alpha=0.5)
 plt.scatter(data[:, 0], data[:, 1], c=target)
 plt.title('Kernel: linear')
-
-
-
 
 plt.show()
